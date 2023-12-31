c4610c999b29e21350b6f87ff1edfdac8210cc6f
GFrasson <bielfc27@gmail.com>
2023-12-31 19:04:34 -0300

    Conexão com calendário - Mensagem de erro e ajustes

--- src/pages/register/connect-calendar/styles.ts
+++ src/pages/register/connect-calendar/styles.ts
@@ -1,19 +1,22 @@
-import { Box, styled } from '@gfrasson-ignite-ui/react'
+import { Box, styled, Text } from '@gfrasson-ignite-ui/react'
 
 export const ConnectBox = styled(Box, {
   marginTop: '$6',
   display: 'flex',
   flexDirection: 'column',
 })
-
 export const ConnectItem = styled('div', {
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'space-between',
-
   border: '1px solid $gray600',
   padding: '$4 $6',
   borderRadius: '$md',
 
-  marginBottom: '$2',
+  marginBottom: '$4',
+})
+
+export const AuthError = styled(Text, {
+  color: '#f75a68',
+  marginBottom: '$4',
 })
